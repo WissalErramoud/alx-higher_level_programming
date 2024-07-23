e3fbf27b2932784410687d7abf001422ec979997
WissalErramoud <WissalErramoud>
2024-07-24 00:11:39 +0100

    0-add_integer.py

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,1 +1,16 @@
 #!/usr/bin/python3
+
+"""Module that contains functions for integer arithmetic"""
+
+
+def add_integer(a, b=98):
+    """Return the addition of two numbers."""
+    if type(a) is not int and type(a) is not float:
+        raise TypeError("a must be an integer")
+    if type(b) is not int and type(b) is not float:
+        raise TypeError("b must be an integer")
+    if type(a) is float:
+        a = int(a)
+    if type(b) is float:
+        b = int(b)
+    return a + b
