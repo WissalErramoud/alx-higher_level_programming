61a6bcb99730a334d8793f67dd5229156d0584c0
WissalErramoud <WissalErramoud>
2024-07-29 10:30:45 +0100

    3-say_my_name.py

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -1,14 +1,12 @@
 #!/usr/bin/python3
-"""
-A function that prints My name is <first name> <last name>
-"""
+
+"""Module that contains functions for personal information"""
 
 
 def say_my_name(first_name, last_name=""):
-    """function that print complete name"""
-    if type(first_name) != str or first_name == "":
+    """Prints 'My name is <first name> <last name>'"""
+    if not isinstance(first_name, str):
         raise TypeError("first_name must be a string")
-    if type(last_name) != str:
+    if not isinstance(last_name, str):
         raise TypeError("last_name must be a string")
-    """print(f"{My name is {first_name} {last_name}")"""
     print("My name is {} {}".format(first_name, last_name))
