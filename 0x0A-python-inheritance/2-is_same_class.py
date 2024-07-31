bd053294b95b335a2b7c36cc152b9d4eb4a821e8
WissalErramoud <WissalErramoud>
2024-08-01 00:55:58 +0100

    2-is_same_class.py

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
-""" Return true if the object is an instance of a class """
+'''Module for is_same_class method.'''
 
 
 def is_same_class(obj, a_class):
-    """
-    Return true if the object is an instance of a class
-    """
+    '''Determines if an object is exactly an instance of a class.'''
     return type(obj) == a_class
