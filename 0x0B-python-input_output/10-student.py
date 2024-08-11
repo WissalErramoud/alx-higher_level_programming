7b70362b15669d383c1fb67404297e4ba81f2b23
WissalErramoud <WissalErramoud>
2024-08-11 21:55:40 +0100

    10-student.py

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -1,27 +1,18 @@
 #!/usr/bin/python3
-"""Creating a Student class."""
+""" creating student class """
 
 
 class Student:
-    """Defining the Student class."""
-
+    """ Defining a class"""
     def __init__(self, first_name, last_name, age):
-        """Initializing a Student instance."""
+        """ Initializing a class"""
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self, attrs=None):
-        """Retrieves a dictionary representation of the instance.
-        
-        Args:
-            attrs (list): Optional list of attribute names to include.
-        
-        Returns:
-            dict: Dictionary representation of the instance.
-        """
-        if isinstance(attrs, list) and all(isinstance(i, str) for i in attrs):
+        """ retrieves a dictionary representation """
+        if (type(attrs)) is list and all(type(i) is str for i in attrs):
             return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
         else:
             return self.__dict__
-
