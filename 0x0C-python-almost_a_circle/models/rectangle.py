d505e8c5d99ee6bfefca4139e9f9c9811e08f286
WissalErramoud <WissalErramoud>
2024-09-13 15:55:16 +0100

    rectangle.py

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,6 +1,7 @@
-#!/usr/bin/python3
+!/usr/bin/python3
 """Creating a rectangle class"""
 from models.base import Base
+
 
 class Rectangle(Base):
     """The class Rectangle that inherits from Base"""
