9df309a8fd51dcfc482baab172d23f0087154de2
WissalErramoud <WissalErramoud>
2024-09-18 16:34:57 +0100

    square.py

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -29,7 +29,7 @@ class Square(Rectangle):
         """Updating square class"""
         if args is not None and len(args) != 0:
             if len(args) >= 1:
-                if type(args[0]) != int and args[0] is not None:
+                if type(args[0]) is not int and args[0] is not None:
                     raise TypeError("id must be an integer")
                 self.id = args[0]
             if len(args) > 1:
@@ -41,7 +41,7 @@ class Square(Rectangle):
         else:
             for key, value in kwargs.items():
                 if key == "id":
-                    if type(value) != int and value is not None:
+                    if type(value) is not int and value is not None:
                         raise TypeError("id must be an integer")
                     self.id = value
                 elif key == "size":
