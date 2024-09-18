b6c3111d93fa966bf35355ca8fc4973c288f805a
WissalErramoud <WissalErramoud>
2024-09-18 15:57:13 +0100

    square.py

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+from models.rectangle import Rectangle
+
+class Square(Rectangle):
+    def __init__(self, size, x=0, y=0, id=None):
+        # Call the super class (Rectangle) with size as both width and height
+        super().__init__(size, size, x, y, id)
+
+    def __str__(self):
+        # Overload the __str__ method to return [Square] (<id>) <x>/<y> - <size>
+        return f"[Square] ({self.id}) {self.x}/{self.y} - {self.width}"
+
+    # Optionally, if you want to have a getter and setter for 'size' to interact more directly
+    @property
+    def size(self):
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        self.width = value
+        self.height = value
+
