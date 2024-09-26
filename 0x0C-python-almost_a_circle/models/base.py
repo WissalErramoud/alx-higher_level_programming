562a971fa7561733ca3658f70cb7693653ad6882
WissalErramoud <124741687+WissalErramoud@users.noreply.github.com>
2024-09-26 13:02:23 +0100

    Update base.py

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -28,25 +28,16 @@ class Base:
             raise TypeError("list_dictionaries must be a list of dictionaries")
         return json.dumps(list_dictionaries)
 
-    @classmethod
+   @classmethod
     def save_to_file(cls, list_objs):
-        """Saving list of objects to file"""
-        if (type(list_objs) is not list and
-           list_objs is not None or
-           not all(isinstance(x, cls) for x in list_objs)):
-            raise TypeError("list_objs must be a list of instances")
-
-        filename = cls.__name__ + ".csv"
+        """Writes the JSON string representation of list_objs to a file."""
+        if list_objs is None or list_objs == []:
+            jstr = "[]"
+        else:
+            jstr = cls.to_json_string([o.to_dictionary() for o in list_objs])
+        filename = cls.__name__ + ".json"
         with open(filename, 'w') as f:
-            if list_objs is not None:
-                list_objs = [x.to_dictionary() for x in list_objs]
-                if cls.__name__ == 'Rectangle':
-                    fields = ['id', 'width', 'height', 'x', 'y']
-                elif cls.__name__ == 'Square':
-                    fields = ['id', 'size', 'x', 'y']
-                writer = csv.DictWriter(f, fieldnames=fields)
-                writer.writeheader()
-                writer.writerows(list_objs)
+            f.write(jstr)
 
     @staticmethod
     def from_json_string(json_string):
@@ -57,14 +48,14 @@ class Base:
 
     @classmethod
     def create(cls, **dictionary):
-        """Creating an instance of class Base"""
+        """Returns an instance with all attributes already set."""
         if cls.__name__ == 'Rectangle':
-            new_instance = cls(1, 1)
+            dummy = cls(1, 1)
         elif cls.__name__ == 'Square':
-            new_instance = cls(1)
-        new_instance.update(**dictionary)
-        return new_instance
-
+            dummy = cls(1)
+        dummy.update(**dictionary)
+        return dummy
+        
     @classmethod
     def load_from_file(cls):
         """Loading list of objects from file"""
@@ -78,52 +69,6 @@ class Base:
                 for d in list_dicts:
                     m.append(cls.create(**d))
         return m
-
-    @classmethod
-    def save_to_file(cls, list_objs):
-        """Writes the JSON string representation of list_objs to a file."""
-        if list_objs is None or list_objs == []:
-            jstr = "[]"
-        else:
-            jstr = cls.to_json_string([o.to_dictionary() for o in list_objs])
-        filename = cls.__name__ + ".json"
-        with open(filename, 'w') as f:
-            f.write(jstr)
-
-    @staticmethod
-    def from_json_string(json_string):
-        """Returns the list of the JSON string representation json_string."""
-        new_len = []
-        if json_string is not None and json_string != '':
-            if type(json_string) is not str:
-                raise TypeError("json_string must be a string")
-            new_len = json.loads(json_string)
-        return new_len
-
-    @classmethod
-    def create(cls, **dictionary):
-        """Returns an instance with all attributes already set."""
-        if cls.__name__ == 'Rectangle':
-            dummy = cls(1, 1)
-        elif cls.__name__ == 'Square':
-            dummy = cls(1)
-        dummy.update(**dictionary)
-        return dummy
-
-    @classmethod
-    def load_from_file(cls):
-        """Returns a list of instances."""
-
-        filename = cls.__name__ + ".json"
-        new_loader = []
-        list_dicts = []
-        if os.path.exists(filename):
-            with open(filename, 'r') as f:
-                s = f.read()
-                list_dicts = cls.from_json_string(s)
-                for d in list_dicts:
-                    new_loader.append(cls.create(**d))
-        return new_loader
 
     @classmethod
     def save_to_file_csv(cls, list_objs):
@@ -190,20 +135,3 @@ class Base:
             Base.draw_rect(t, i)
             time.sleep(1)
         time.sleep(5)
-
-    @staticmethod
-    def draw_rect(t, rect):
-        """Helper method that draws a Rectangle
-        or Square.
-        """
-
-        t.penup()
-        t.setpos(rect.x, rect.y)
-        t.pendown()
-        t.forward(rect.width)
-        t.left(90)
-        t.forward(rect.height)
-        t.left(90)
-        t.forward(rect.width)
-        t.left(90)
-        t.forward(rect.height)
